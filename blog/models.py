f9a69bf118a492c8f737768a9aa84f3287bec9d6
NadyaEvp <t_evp@mail.ru>
2024-10-05 18:39:18 +0300

    use static template tag

--- blog/models.py
+++ blog/models.py
@@ -14,34 +14,29 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         verbose_name="Автор",
-        limit_choices_to={'is_staff': True})
+        limit_choices_to={"is_staff": True},
+    )
     likes = models.ManyToManyField(
-        User,
-        related_name="liked_posts",
-        verbose_name="Кто лайкнул",
-        blank=True)
+        User, related_name="liked_posts", verbose_name="Кто лайкнул", blank=True
+    )
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post_detail', args={'slug': self.slug})
+        return reverse("post_detail", args={"slug": self.slug})
 
     class Meta:
-        ordering = ['-published_at']
-        verbose_name = 'пост'
-        verbose_name_plural = 'посты'
+        ordering = ["-published_at"]
+        verbose_name = "пост"
+        verbose_name_plural = "посты"
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        "Post",
-        on_delete=models.CASCADE,
-        verbose_name="Пост, к которому написан")
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name="Автор")
+        "Post", on_delete=models.CASCADE, verbose_name="Пост, к которому написан"
+    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
 
     text = models.TextField("Текст комментария")
     published_at = models.DateTimeField("Дата и время публикации")
@@ -50,6 +45,6 @@ class Comment(models.Model):
         return f"{self.author.username} under {self.post.title}"
 
     class Meta:
-        ordering = ['published_at']
-        verbose_name = 'комментарий'
-        verbose_name_plural = 'комментарии'
+        ordering = ["published_at"]
+        verbose_name = "комментарий"
+        verbose_name_plural = "комментарии"
--- blog/views.py
+++ blog/views.py
@@ -25,15 +25,17 @@ def index(request):
     """
     Вьюхи не оптимизированы, потому что в последней задаче модуля Django ORM нужно их оптимизировать как раз на примере этого сайта.
     """
-    all_posts = Post.objects.prefetch_related('author')
-    popular_posts = all_posts.annotate(likes_count=Count('likes')).order_by('-likes_count')[:3]
-    fresh_posts = all_posts.order_by('-published_at')[:5]
+    all_posts = Post.objects.prefetch_related("author")
+    popular_posts = all_posts.annotate(likes_count=Count("likes")).order_by(
+        "-likes_count"
+    )[:3]
+    fresh_posts = all_posts.order_by("-published_at")[:5]
 
     context = {
-        'most_popular_posts': [serialize_post(post) for post in popular_posts],
-        'fresh_posts': [serialize_post(post) for post in fresh_posts],
+        "most_popular_posts": [serialize_post(post) for post in popular_posts],
+        "fresh_posts": [serialize_post(post) for post in fresh_posts],
     }
-    return render(request, 'index.html', context)
+    return render(request, "index.html", context)
 
 
 def post_detail(request, slug):
@@ -44,27 +46,29 @@ def post_detail(request, slug):
     comments = Comment.objects.filter(post=post)
     serialized_comments = []
     for comment in comments:
-        serialized_comments.append({
-            'text': comment.text,
-            'published_at': comment.published_at,
-            'author': comment.author.username,
-        })
+        serialized_comments.append(
+            {
+                "text": comment.text,
+                "published_at": comment.published_at,
+                "author": comment.author.username,
+            }
+        )
 
     serialized_post = {
         "title": post.title,
         "text": post.text,
         "author": post.author.username,
         "comments": serialized_comments,
-        'likes_amount': post.likes.count(),
+        "likes_amount": post.likes.count(),
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
     }
 
     context = {
-        'post': serialized_post,
+        "post": serialized_post,
     }
-    return render(request, 'blog-details.html', context)
+    return render(request, "blog-details.html", context)
 
 
 def contact(request):
@@ -79,4 +83,4 @@ def contact(request):
         tooltip="Мы здесь",
     ).add_to(folium_map)
     html_map = folium_map._repr_html_()
-    return render(request, 'contact.html', {"html_map": html_map})
+    return render(request, "contact.html", {"html_map": html_map})
